6c45a91884ded1bc4b3117b4a6f534bcf0e5fd53
priyankasekarcollab <57703440+priyankasekarcollab@users.noreply.github.com>
2025-07-10 21:46:58 +0530

    Add files via upload

---
+++ Interface/DatabseConnection.java
@@ -0,0 +1,8 @@
+package week3.Interface;
+
+public interface DatabseConnection {
+	public void connect();
+	public void disconnect();
+	public void executeUpdate();
+
+}
---
+++ Interface/JavaConnection.java
@@ -0,0 +1,26 @@
+package week3.Interface;
+
+public class JavaConnection implements DatabseConnection{
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void executeUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
